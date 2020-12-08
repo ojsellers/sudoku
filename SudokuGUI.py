f5394849792c235d0a717175edb82b6b832c51ca
ojsellers <olliejsellers@gmail.com>
2020-12-08 18:39:56 +0000

    Style updates

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -8,8 +8,11 @@ import sys
 from SudokuGenerator import *
 
 class gui():
-    '''Initialise all variables that define the gui'''
+
     def __init__(self):
+        """
+        Initialise all variables that define the gui
+        """
         self.win_size = 130
         self.win_mult = 5
         self.win_ext = 200
@@ -32,73 +35,99 @@ class gui():
         self.h = 100
         self.grid = np.zeros((9, 9))
 
+        
     def draw_lines(self):
         for i in range(0, self.win_width - self.win_ext, self.cell):
             pygame.draw.line(self.display, self.grey,
                              (i, 0), (i, self.win_height))
             pygame.draw.line(self.display, self.grey,
                              (0, i), (self.win_width - self.win_ext, i))
+        
         for j in range(0, self.win_width, self.square_size):
             pygame.draw.line(self.display, self.black,
                              (j, 0), (j, self.win_height))
             pygame.draw.line(self.display, self.black,
                              (0, j), (self.win_width - self.win_ext, j))
 
+            
     def draw_numbers(self):
         font = pygame.font.SysFont('calibri', 45)
+        
         for i in range(9):
             for j in range(9):
                 if self.grid[i][j] != 0:
                     number = font.render(str(int(self.grid[i][j])),
                                          False, self.black)
+                    
                     num_w = number.get_rect().width
                     num_h = number.get_rect().height
+                    
                     self.display.blit(number,
                                       ((self.cell - num_w) / 2 + self.cell * j,
                                        (self.cell - num_h)/ 2 + self.cell * i))
+                    
                     pygame.display.update()
                     time.sleep(3 / 81)
 
-    '''A function to draw a simple rectangular pygame button of a desired size
-    and location with text on it'''
+                    
     def draw_button(self, colour, text, x, y, w, h):
-        font = pygame.font.SysFont('calibri', 25, bold = True)
+        """
+        A function to draw a simple rectangular pygame button of a desired size
+        and location with text on it
+        """
         pygame.draw.rect(self.display, colour, (x, y, w, h))
+        
+        font = pygame.font.SysFont('calibri', 25, bold = True)
         textsurface = font.render(text, False, self.blue)
         word_width = textsurface.get_rect().width
+        
         self.display.blit(textsurface, (x + (w - word_width) / 2, y + h / 3))
 
-    '''This function draws the particular buttons for this sudoku generator'''
+        
     def draw_buttons(self):
+        """
+        This function draws the particular buttons for this sudoku generator
+        """
         names = ['New Easy', 'New Medium', 'New Hard', 'Solve It']
+        
         for i in range(4):
             self.draw_button(self.hot_pink, names[i], self.x,
                              self.y + i * 130, self.w, self.h)
 
-    '''when a mouse click occurs on a button, this function is called and
-    performs a routine depending on which button was clicked'''
+
     def click_funct(self, nums_to_remove, y, method, i):
+        """
+        when a mouse click occurs on a button, this function is called and
+        performs a routine depending on which button was clicked
+        """
         self.draw_button(self.green, 'Wait...', self.x,
                          y, self.w, self.h)
         pygame.display.update()
+        
         if method == 'g':
             self.setup()
             new = sudoku_generator(np.zeros((9, 9)))
             new.gen_reasonable_time(nums_to_remove, 0.1)
             self.grid = new.grid
             self.draw_numbers()
+        
         if method == 's':
             new = sudoku_solverAI(self.grid)
             new.solve('single_soln')
             new.update_grid() #added this line for ai
             self.grid = new.grid
             self.draw_numbers()
+        
         self.draw_buttons()
 
-    '''This a function to determine when and where a button click is'''
+
     def button_click(self):
+        """
+        This a function to determine when and where a button click is
+        """
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
+        
         if click[0] == 1:
             for i in range(4):
                 if (self.x + self.w > mouse[0] > self.x and
@@ -119,8 +148,11 @@ class gui():
         else:
             return
 
-    '''This function can be called to set up the pygame window'''
+
     def setup(self):
+        """
+        This function can be called to set up the pygame window
+        """
         self.fpsclock = pygame.time.Clock()
         self.fpsclock.tick(self.fps)
         self.display = pygame.display.set_mode((self.win_width,
